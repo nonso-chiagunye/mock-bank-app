34563b25e83e5e09e3eda4ef4d0be5ebac2300dd
nonso-chiagunye <nonsoasoby@hotmail.com>
2023-09-22 13:52:34 +0400

    updated welcome message

--- script.js
+++ script.js
@@ -359,6 +359,7 @@ btnClose.addEventListener('click', function (e) {
 
     // Hide UI
     containerApp.style.opacity = 0;
+    labelWelcome.textContent = 'Log in to get started';
   }
 
   inputCloseUsername.value = inputClosePin.value = '';
